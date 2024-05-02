6169e6f347ed35e34a0a21723f02272ffb2d6947
jamesonhm <jamesonhmarshall@gmail.com>
2024-05-02 06:11:06 -0600

    add dtz module for timezone stuff

---
+++ backend/dtz.py
@@ -0,0 +1,115 @@
+from datetime import tzinfo, timedelta, datetime
+"""
+https://docs.python.org/3/library/datetime.html#datetime.tzinfo
+"""
+
+ZERO = timedelta(0)
+HOUR = timedelta(hours=1)
+
+def first_sunday_on_or_after(dt):
+    days_to_go = 6 - dt.weekday()
+    if days_to_go:
+        dt += timedelta(days_to_go)
+    return dt
+
+
+# US DST Rules
+#
+# This is a simplified (i.e., wrong for a few cases) set of rules for US
+# DST start and end times. For a complete and up-to-date set of DST rules
+# and timezone definitions, visit the Olson Database (or try pytz):
+# http://www.twinsun.com/tz/tz-link.htm
+# https://sourceforge.net/projects/pytz/ (might not be up-to-date)
+#
+# In the US, since 2007, DST starts at 2am (standard time) on the second
+# Sunday in March, which is the first Sunday on or after Mar 8.
+DSTSTART_2007 = datetime(1, 3, 8, 2)
+# and ends at 2am (DST time) on the first Sunday of Nov.
+DSTEND_2007 = datetime(1, 11, 1, 2)
+# From 1987 to 2006, DST used to start at 2am (standard time) on the first
+# Sunday in April and to end at 2am (DST time) on the last
+# Sunday of October, which is the first Sunday on or after Oct 25.
+DSTSTART_1987_2006 = datetime(1, 4, 1, 2)
+DSTEND_1987_2006 = datetime(1, 10, 25, 2)
+# From 1967 to 1986, DST used to start at 2am (standard time) on the last
+# Sunday in April (the one on or after April 24) and to end at 2am (DST time)
+# on the last Sunday of October, which is the first Sunday
+# on or after Oct 25.
+DSTSTART_1967_1986 = datetime(1, 4, 24, 2)
+DSTEND_1967_1986 = DSTEND_1987_2006
+
+def us_dst_range(year):
+    if 2006 < year:
+        dststart, dstend = DSTSTART_2007, DSTEND_2007
+    elif 1986 < year < 2007:
+        dststart, dstend = DSTSTART_1987_2006, DSTEND_1987_2006
+    elif 1966 < year < 1987:
+        dststart, dstend = DSTSTART_1967_1986, DSTEND_1967_1986
+    else:
+        return (datetime(year, 1, 1), ) * 2
+
+    start = first_sunday_on_or_after(dststart.replace(year=year))
+    end = first_sunday_on_or_after(dstend.replace(year=year))
+    return start, end
+
+
+class USTimeZone(tzinfo):
+
+    def __init__(self, stdoffset, reprname, stdname, dstname):
+        self.stdoffset = timedelta(hours=stdoffset)
+        self.reprname = reprname
+        self.stdname = stdname
+        self.dstname = dstname
+
+    def __repr__(self):
+        return self.reprname
+
+    def tzname(self, dt):
+        if self.dst(dt):
+            return self.dstname
+        else:
+            return self.stdname
+
+    def utcoffset(self, dt):
+        return self.stdoffset + self.dst(dt)
+
+    def dst(self, dt):
+        if dt is None or dt.tzinfo is None:
+            # raise ValueError here?
+            return ZERO
+        # raise valueError here?
+        assert dt.tzinfo is self
+        start, end = us_dst_range(dt.year)
+        # can't compare naive to aware objects, so strip tz from dt first
+        dt = dt.replace(tzinfo=None)
+        if start + HOUR <= dt < end - HOUR:
+            # DST in effect
+            return HOUR
+        if end - HOUR <= dt < end:
+            #Fold (an ambiguous hour): use dt.fold to disambiguate
+            return ZERO if dt.fold else HOUR
+        if start <= dt < start + HOUR:
+            # Gap (a non-existent hour): reverse the fold rule
+            return HOUR if dt.fold else ZERO
+        # DST is off
+        return ZERO
+
+    def fromutc(self, dt:datetime):
+        assert dt.tzinfo is self
+        start, end = us_dst_range(dt.year)
+        start = start.replace(tzinfo=self)
+        end = end.replace(tzinfo=self)
+        std_time = dt + self.stdoffset
+        dst_time = std_time + HOUR
+        if end <= dst_time < end + HOUR:
+            # repeated an hour
+            return std_time.replace(fold=1)
+        if std_time < start or dst_time >= end:
+            # standart time 
+            return std_time
+        if start <= std_time < end - HOUR:
+            # daylight savings time 
+            return dst_time
+
+Eastern = USTimeZone(-5, "Eastern", "EST", "EDT")
+
