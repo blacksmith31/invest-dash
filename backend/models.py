e0588bed995ced4e93af96a57b64863ff9554aed
jamesonhm <jamesonhmarshall@gmail.com>
2024-05-07 06:36:05 -0600

    add tracking status enum

--- backend/models.py
+++ backend/models.py
@@ -1,19 +1,31 @@
+from enum import Enum
 from pydantic import BaseModel, Field
 
+
+class TrackingStatus(Enum):
+    AutoUntracked = 0
+    AutoTracked = 1
+    ManualUntracked = 2
+    ManualTracked = 3
+    
 
 class TickerDay(BaseModel):
     ticker: str = Field(max_length=7)
     ts: float
 
+
 class TickerDayClose(TickerDay):
     close: float = Field(gt=0)
+
 
 class TickerDayScore(TickerDay):
     score: float = Field(default=0, alias="sroc")
 
+
 class TickerDayScoredClose(TickerDay):
     close: float = Field(gt=0)
     score: float = Field(default=0, alias="sroc")
+
 
 class Symbol(BaseModel):
     symbol: str = Field(max_length=7)
@@ -23,7 +35,9 @@ class Symbol(BaseModel):
     industry: str
     sector: str
 
+
 class UserSymbol(Symbol):
-    ### TODO Replace with tracking enum
-    tracking: int
+    tracking: TrackingStatus = TrackingStatus.AutoUntracked
     owned: bool
+
+
