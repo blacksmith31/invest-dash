b719f5e52d75e9eeb7cf984841abea138d2bfb45
jamesonhm <jamesonhmarshall@gmail.com>
2024-05-15 06:41:03 -0600

    add strat test

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -7,16 +7,22 @@ from schemas.strategy import SROCStrategy
 result = select_ticker_closes('META')
 limited_set = [day for day in result if day["timestamp"] <= 1715347800]
 
-strats = [
+sroc_strats = [
     SROCStrategy(scored_tickers=10, top_tickers=5, ema_window=13, roc_window=90)
 ]
 expected_scores = [
-    34.3
+    34.18
+]
+expected_qdays = [
+    126
 ]
 
-@pytest.mark.parametrize("strat, expected_score", list(zip(strats, expected_scores)))
-def test_score(strat, expected_score):
+@pytest.mark.parametrize("strat, expected_score", list(zip(sroc_strats, expected_scores)))
+def test_sroc_score(strat, expected_score):
     scores = strat.score(limited_set)
     # print(f"scores: {scores[-20:]}")
-    assert expected_score == approx(scores[-1]["sroc"], rel=0.5)
+    assert expected_score == approx(scores[-1]["sroc"], abs=0.5)
      
+@pytest.mark.parametrize("strat, expected_qday", list(zip(sroc_strats, expected_qdays)))
+def test_post_init(strat, expected_qday):
+    assert strat.query_days == expected_qday
